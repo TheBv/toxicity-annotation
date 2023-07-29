97fd9fa1eeded3ca846b9519ac2968a2c153b592
Patrick Schrottenbacher <bv939@web.de>
2023-07-29 18:43:12 +0200

    fix(devops): fixed backend ports and urls

--- src/main/webapp/src/lib/constants.ts
+++ src/main/webapp/src/lib/constants.ts
@@ -1,7 +1,6 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL =  "http://toxicity.demoticks.tf:8082"//process.env.BACKEND_URL || "http://localhost:8080";
 //const BASE_URL = "";
 //const BASE_URL = "http://twitterannotator.texttechnologylab.org";
-
 export {
     BASE_URL
 };
